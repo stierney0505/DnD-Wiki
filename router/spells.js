a5cb3e6002bb6e096f546cda6b849aef887621b3
Sean Tierney <stierney0505@gmail.com>
2023-10-01 20:27:55 -0400

    Forgot to Fix Routes in spell.js

--- router/spells.js
+++ router/spells.js
@@ -1,40 +1,39 @@
 const express = require('express');
-require('dotenv').config();
 const router = express.Router();
-const path = process.env.__PATH;
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
 
 router.get('/', (req, res) => {
-    res.render(path + '/views/spellViews/spells-home-page.ejs');
+    res.render(path.join(__dirname, '../views/spellViews/spells-home-page.ejs'));
 })
 
 router.get('/schools', (req, res) => {
-    res.render(path + '/views/spellViews/spells-by-school.ejs');
+    res.render(path.join(__dirname, '../views/spellViews/spells-by-school.ejs'));
 });
 
 router.get('/schools:school', (req, res) => {
     let school = req.params.school.substring(1);
-    res.render(path + '/views/spellViews/spells-school-page.ejs', {school: school});
+    res.render(path.join(__dirname, '../views/spellViews/spells-school-page.ejs'), {school: school});
 });
 
 router.get('/levels', (req, res) => {
-    res.render(path + '/views/spellViews/spells-by-level.ejs');
+    res.render(path.join(__dirname, '../views/spellViews/spells-by-level.ejs'));
 });
 
 router.get('/levels:level', (req, res) => {
     let level = req.params.level.substring(1);
-    res.render(path + '/views/spellViews/spells-level-page.ejs', {level: level});
+    res.render(path.join(__dirname, '../views/spellViews/spells-level-page.ejs'), {level: level});
 });
 
 router.get('/classes', (req, res) => {
-    res.render(path + '/views/spellViews/spells-by-class.ejs');
+    res.render(path.join(__dirname, '../views/spellViews/spells-by-class.ejs'));
 });
 
 router.get('/classes:class', (req, res) => {
     let theClass = req.params.class.substring(1);
-    res.render(path + '/views/spellViews/spells-class-page.ejs', {theClass: theClass});
+    res.render(path.join(__dirname, '../views/spellViews/spells-class-page.ejs'), {theClass: theClass});
 });
 
 module.exports.spell_router = router;
